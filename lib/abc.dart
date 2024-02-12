2ce5cc439713e90134ded11652b6655a2a95ca9d
fatima <fatima031>
2024-02-12 23:45:48 +0500

    practice

---
+++ lib/abc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Classes_practice extends StatelessWidget {
+  const Classes_practice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TextField(),
+    );
+  }
+}
